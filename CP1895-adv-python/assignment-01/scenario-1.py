133779ebe8a63f7206bc4b158698156831cc11e9
Andrew Payne <andrewmdpayne@gmail.com>
2024-10-26 21:25:26 -0230

    Update
    Added main() block.

--- CP1895-adv-python/assignment-01/scenario-1.py
+++ CP1895-adv-python/assignment-01/scenario-1.py
@@ -46,6 +46,24 @@ class Square(Rectangle):
     def __str__(self):
         return f"This square has a length of {self.length}."
 
+def main():
+    print("Rectangle Calculator\n")
+    choice = True
+    while choice:
+        shapeChoice = input("Rectangle or square? (r/s): ")
+        if (shapeChoice.lower() == "r"):
+            continue
+            # Do rectangle stuff
+        elif (shapeChoice.lower() == "s"):
+            continue
+            # Do square stuff
+        
+
+
+if __name__ == "__main__":
+    main()
+
+
 """
 r1 = Rectangle(3, 10)
 print(r1)
