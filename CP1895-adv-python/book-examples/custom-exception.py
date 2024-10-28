e31e2e83eb597f9e0f87ee68db915b9896d38fe7
amdp64 <andrew.payne64@ed.cna.nl.ca>
2024-10-28 16:16:06 -0230

    Update to custom exception
    Module added which uses DataAccessError. Module used to handle a custom DataAccessError exception.

--- CP1895-adv-python/book-examples/custom-exception.py
+++ CP1895-adv-python/book-examples/custom-exception.py
@@ -1,2 +1,27 @@
+# Class that defines a custom exception.
 class DataAccessError(Exception):
     pass
+
+# Module that uses the DataAccessError class
+from objects import DataAccessError
+
+def read_movies():
+    try:
+        movies = []
+        with open("movies.csv", newline="") as file:
+            reader = csv.reader(file)
+            for now in reader:
+                movies.append(row)
+        return movies
+    except FileNotFoundError:
+        raise DataAccessError("Data source not found.")
+    except Exception:
+        raise DataAccessError("Error accessing data source.")
+
+# Handles a custom exception
+from objects import DataAccessError
+
+try:
+    movies = db.read_movies()
+except DataAccessError as e:
+    print("DataAccessError:", e)
