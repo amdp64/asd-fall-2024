75090719c26a796cfed2317c53aff326e819313c
Andrew Payne <andrewmdpayne@gmail.com>
2024-10-30 21:24:05 -0230

    Scenario-2 Added
    Initial commit. Code for assignment 1 scenario 2.

---
+++ CP1895-adv-python/assignment-01/scenario-2.py
@@ -0,0 +1,80 @@
+# Customer/Employee Data Entry
+
+# Has a Person class that provides attributes for:
+# first name, last name, and email.
+
+# Has a Customer class that inherits Person, and adds:
+# An attribute for a customer number
+
+# Has an Employee class that inherits Person, and adds:
+# An attribute for a social security number (SSN)
+
+class Person:
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+    def getFullName(self):
+        return self.first_name + " " + self.last_name
+
+class Customer(Person):
+    def __init__(self, first_name, last_name, email, customer_no):
+        self.customer_no = customer_no
+        super().__init__(first_name, last_name, email)
+
+class Employee(Person):
+    def __init__(self, first_name, last_name, email, ssn):
+        self.ssn = ssn
+        super().__init__(first_name, last_name, email)
+
+newObject = None
+
+def main():
+    print("Customer/Employee Data Entry\n")
+    while True:
+        choice = input("Customer or employee (c/e): ")
+        global newObject
+        if (choice.lower() == 'c'):
+            print("DATA ENTRY")
+            fname = input("First name: ")
+            lname = input("Last name: ")
+            email = input("Email: ")
+            cust_no = input("Number: ")
+            # Create a new Customer object
+            newObject = Customer(fname.capitalize(), lname.capitalize(), email, cust_no)
+            break
+        elif (choice.lower() == 'e'):
+            print("DATA ENTRY")
+            fname = input("First name: ")
+            lname = input("Last name: ")
+            email = input("Email: ")
+            ssn = input("SSN: ")
+            # Create a new Employee object
+            newObject = Employee(fname.capitalize(), lname.capitalize(), email, ssn)
+            break
+        else:
+            print("Unknown command. Please try again.")
+    # Look at object created.
+    # Display the data to the user.
+    while True:
+        if (isinstance(newObject, Customer)):
+            print("\nCUSTOMER")
+            print(f"Name:\t\t{newObject.getFullName()}")
+            print(f"Email:\t\t{newObject.email}")
+            print(f"Number:\t\t{newObject.customer_no}")
+        
+        elif (isinstance(newObject, Employee)):
+            print("\nEmployee")
+            print(f"Name:\t\t{newObject.getFullName()}")
+            print(f"Email:\t\t{newObject.email}")
+            print(f"SSN:\t\t{newObject.ssn}")
+
+        choice = input("\nContinue? (y/n): ")
+        if (choice.lower() == 'n'):
+            break
+
+print("\nBye!")
+
+if __name__ == "__main__":
+    main()
