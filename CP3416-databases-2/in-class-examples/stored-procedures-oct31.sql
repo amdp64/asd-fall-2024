9fad393482af8c0e04296b9d8a254cbe6673351d
amdp64 <andrew.payne64@ed.cna.nl.ca>
2024-10-31 10:34:58 -0230

    Update stored procedure example
    Update to include Eric's solution.

--- CP3416-databases-2/in-class-examples/stored-procedures-oct31.sql
+++ CP3416-databases-2/in-class-examples/stored-procedures-oct31.sql
@@ -15,3 +15,32 @@ DELIMITER ;
 
 DROP PROCEDURE IF EXISTS test;
 CALL test();
+
+-- Eric's solution.
+
+DELIMITER //
+
+CREATE PROCEDURE exercise1()
+BEGIN
+	DECLARE num_invoices INT;
+
+    DECLARE invoices_cursor CURSOR FOR
+		SELECT COUNT(*) FROM invoices
+        WHERE invoice_total - payment_total - credit_total >= 5000;
+	
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		SELECT "SQLERROR occured, procedure exited.";
+	
+    OPEN invoices_cursor;
+        
+	FETCH invoices_cursor INTO num_invoices;
+    
+    SELECT CONCAT(num_invoices, " invoices exceeds $5,000.");
+    
+    CLOSE invoices_cursor;
+END
+//
+
+DELIMITER ;
+
+CALL exercise1();
